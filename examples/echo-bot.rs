1eb198872ac001127a412569c22b329d0569bd21
Michael Zhang <iptq@protonmail.com>
2019-12-03 21:01:59 -0600

    Osu

--- examples/echo-bot.rs
+++ examples/echo-bot.rs
@@ -1,23 +1,35 @@
 use anyhow::Result;
 use futures::stream::StreamExt;
-use irc_async::{ClientError, Config, Client};
+use futures::future;
+use irc_async::{Client, ClientError, Config};
 
-async fn run() -> Result<()> {
-    let config = Config {
-        host: "chat.freenode.net".into(),
-        port: 6697,
-        ssl: true,
-        nick: "hello".into(),
-    };
-    let mut client = Client::with_config(config).await?;
-    client.register().await;
-
+async fn l(mut client: Client) -> Result<()> {
     loop {
         let message = match client.next().await {
             Some(message) => message,
             None => break,
         };
     }
+    Ok(())
+}
+
+async fn run() -> Result<()> {
+    // let config = Config {
+    //     host: "chat.freenode.net".into(),
+    //     port: 6697,
+    //     ssl: true,
+    //     nick: "hello".into(),
+    // };
+    let config = Config {
+        host: "127.0.0.1".into(),
+        port: 4444,
+        ssl: false,
+        nick: "hello".into(),
+    };
+    let (mut client, osu) = Client::with_config(config).await?;
+    client.register().await;
+
+    future::join(osu, l(client)).await;
 
     unreachable!()
 }
--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,14 +1,17 @@
 mod config;
+mod stream;
 
 use std::io;
 use std::net::ToSocketAddrs;
 use std::pin::Pin;
-use std::sync::mpsc::{self, Receiver, Sender};
+use std::sync::{Arc, Mutex};
 use std::task::{Context, Poll};
 
 use anyhow::Result;
+use futures::future::{self, Either, Future, FutureExt, Ready};
+use futures::channel::mpsc::{self, UnboundedSender};
 use futures::sink::SinkExt;
-use futures::stream::{SplitSink, SplitStream, Stream, StreamExt};
+use futures::stream::{FilterMap, SplitSink, SplitStream, Stream, StreamExt};
 use native_tls::TlsConnector;
 use tokio::io::{AsyncRead, AsyncWrite};
 use tokio::net::TcpStream;
@@ -16,6 +19,7 @@ use tokio::prelude::*;
 use tokio_tls::{TlsConnector as TokioTlsConnector, TlsStream};
 use tokio_util::codec::{Decoder, Framed};
 
+use crate::client::stream::ClientStream;
 use crate::proto::{Command, IrcCodec, Message};
 
 pub use self::config::Config;
@@ -30,13 +34,14 @@ pub enum ClientError {
 
 pub struct Client {
     config: Config,
-    stream: SplitStream<Framed<ClientStream, IrcCodec>>,
-    sink: SplitSink<Framed<ClientStream, IrcCodec>, Message>,
-    filter_rx: Receiver<Message>,
+    stream: Pin<Box<dyn Stream<Item = Result<Message>>>>,
+    tx: UnboundedSender<Message>,
 }
 
+pub type Osu = Pin<Box<dyn Future<Output=Result<()>> + Send>>;
+
 impl Client {
-    pub async fn with_config(config: Config) -> Result<Self> {
+    pub async fn with_config(config: Config) -> Result<(Self, Osu)> {
         let mut addrs = (config.host.as_ref(), config.port).to_socket_addrs()?;
         let mut stream = TcpStream::connect(addrs.next().unwrap()).await?;
 
@@ -47,25 +52,49 @@ impl Client {
         } else {
             ClientStream::Plain(stream)
         };
+
         let stream = IrcCodec::new().framed(stream);
         let (sink, stream) = stream.split();
-        let (filter_tx, filter_rx) = mpsc::channel();
+        let (tx, filter_rx) = mpsc::unbounded();
+        let mut filter_tx = tx.clone();
+
+        let stream = stream.filter_map(move |message| {
+            if let Ok(Message {
+                command: Command::PING(code, _),
+                ..
+            }) = message
+            {
+                let mut filter_tx = filter_tx.clone();
+                Either::Left(async move {
+                    filter_tx.send(Message {
+                        tags: None,
+                        prefix: None,
+                        command: Command::PONG(code, None),
+                    }).await;
+                    None
+                })
+            } else {
+                Either::Right(future::ready(Some(message)))
+            }
+        });
+
+        let osu = filter_rx.map(Ok).forward(sink).boxed();
+
         let client = Client {
             config,
-            stream,
-            sink,
-            filter_rx,
+            stream: stream.boxed(),
+            tx,
         };
-        Ok(client)
+        Ok((client, osu))
     }
 
-    pub async fn register(&mut self) {
+    pub async fn register(&mut self) -> Result<()> {
         self.send(Message {
             tags: None,
             prefix: None,
             command: Command::NICK(self.config.nick.clone()),
         })
-        .await;
+        .await?;
         self.send(Message {
             tags: None,
             prefix: None,
@@ -75,12 +104,13 @@ impl Client {
                 self.config.nick.clone(),
             ),
         })
-        .await;
+        .await
     }
 
-    pub async fn send(&mut self, message: Message) {
-        self.sink.send(message).await;
-        self.sink.flush().await;
+    pub async fn send(&mut self, message: Message) -> Result<()> {
+        self.tx.send(message).await?;
+        self.tx.flush().await?;
+        Ok(())
     }
 }
 
@@ -89,61 +119,5 @@ impl Stream for Client {
 
     fn poll_next(self: Pin<&mut Self>, context: &mut Context) -> Poll<Option<Self::Item>> {
         Stream::poll_next(Pin::new(&mut self.get_mut().stream), context)
-    }
-}
-
-enum ClientStream {
-    Plain(TcpStream),
-    Tls(TlsStream<TcpStream>),
-}
-
-impl AsyncRead for ClientStream {
-    fn poll_read(
-        self: Pin<&mut Self>,
-        context: &mut Context,
-        buf: &mut [u8],
-    ) -> Poll<Result<usize, io::Error>> {
-        match self.get_mut() {
-            ClientStream::Plain(stream) => TcpStream::poll_read(Pin::new(stream), context, buf),
-            ClientStream::Tls(stream) => {
-                TlsStream::<TcpStream>::poll_read(Pin::new(stream), context, buf)
-            }
-        }
-    }
-}
-
-impl AsyncWrite for ClientStream {
-    fn poll_write(
-        self: Pin<&mut Self>,
-        context: &mut Context,
-        buf: &[u8],
-    ) -> Poll<Result<usize, io::Error>> {
-        match self.get_mut() {
-            ClientStream::Plain(stream) => TcpStream::poll_write(Pin::new(stream), context, buf),
-            ClientStream::Tls(stream) => {
-                TlsStream::<TcpStream>::poll_write(Pin::new(stream), context, buf)
-            }
-        }
-    }
-
-    fn poll_flush(self: Pin<&mut Self>, context: &mut Context<'_>) -> Poll<Result<(), io::Error>> {
-        match self.get_mut() {
-            ClientStream::Plain(stream) => TcpStream::poll_flush(Pin::new(stream), context),
-            ClientStream::Tls(stream) => {
-                TlsStream::<TcpStream>::poll_flush(Pin::new(stream), context)
-            }
-        }
-    }
-
-    fn poll_shutdown(
-        self: Pin<&mut Self>,
-        context: &mut Context<'_>,
-    ) -> Poll<Result<(), io::Error>> {
-        match self.get_mut() {
-            ClientStream::Plain(stream) => TcpStream::poll_shutdown(Pin::new(stream), context),
-            ClientStream::Tls(stream) => {
-                TlsStream::<TcpStream>::poll_shutdown(Pin::new(stream), context)
-            }
-        }
     }
 }
