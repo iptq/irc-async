23448a860fd97e8f1382ac4f704ce102781655c1
Michael Zhang <iptq@protonmail.com>
2019-12-03 21:49:06 -0600

    yay

--- examples/echo-bot.rs
+++ examples/echo-bot.rs
@@ -1,7 +1,8 @@
-use anyhow::Result;
-use futures::future;
+use futures::future::{self, FutureExt};
 use futures::stream::StreamExt;
 use irc_async::{Client, ClientError, Config};
+
+type Result<T> = std::result::Result<T, ClientError>;
 
 async fn run() -> Result<()> {
     let config = Config {
@@ -11,16 +12,16 @@ async fn run() -> Result<()> {
         nick: "hello".into(),
     };
     let (mut client, osu) = Client::with_config(config).await?;
-    client.register().await;
+    client.register().await?;
 
     let handler = async {
         while let Some(Ok(message)) = client.next().await {
             println!("message: {:?}", message);
-            client.send(message);
+            client.send(message).await.unwrap();
         }
     };
 
-    future::join(osu, handler).await;
+    future::join(osu, handler).map(|(first, _)| first).await?;
 
     Ok(())
 }
